f39f33080c3cc66150760940af74b76ffc9aac33
loyd_ <loyd_pintac@yahoo.com>
2022-04-29 21:24:57 -0500

    Adding POJOs. Closes issue #2.

---
+++ src/main/java/guru/springframework/sfgpetclinic/model/PetType.java
@@ -0,0 +1,17 @@
+package guru.springframework.sfgpetclinic.model;
+
+/**
+ * @Project sfg-pet-clinic
+ * @Author loyd_ on 4/29/2022
+ **/
+public class PetType {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ src/main/java/guru/springframework/sfgpetclinic/model/Vet.java
@@ -0,0 +1,9 @@
+package guru.springframework.sfgpetclinic.model;
+
+/**
+ * @Project sfg-pet-clinic
+ * @Author loyd_ on 4/29/2022
+ **/
+public class Vet extends Person {
+
+}
